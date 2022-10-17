680a9eeef5313e5570b675a1612e61d39765f54a
Beno <62211551+notjawad@users.noreply.github.com>
2022-10-17 05:12:58 -0500

    wider search bar

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
             <div className="h-24">
                 <div className="box col-start-1 col-span-4 flex items-center justify-center">
                     <div className="flex flex-col gap-2 my-4">
-                        <h1 className="text-center">
+                        <h1 className="text-center text-bold">
                             Rick & Morty{" "}
                             <span className="text-blue-400">Database</span>
                         </h1>
